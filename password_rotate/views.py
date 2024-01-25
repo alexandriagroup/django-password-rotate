0aded0e387a369eac3574017847d9ed4cc15e81f
damien.rabois <damien.rabois@abc-culture.fr>
2024-01-25 14:39:06 +0100

    chore: clean `ForcePasswordChangeView`

--- password_rotate/views.py
+++ password_rotate/views.py
@@ -3,17 +3,10 @@ from django.contrib.auth.views import PasswordChangeView
 
 
 class ForcePasswordChangeView(PasswordChangeView):
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs["user"] = self.request.user
-        self.request.password_changed = None
-        return kwargs
-
     def form_valid(self, form):
         form.save()
         # Updating the password logs out all other sessions for the user
         # except the current one.
         update_session_auth_hash(self.request, form.user)
         self.request.password_status = "valid"
-        print("password_changed set to True")
         return super().form_valid(form)
