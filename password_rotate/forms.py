720d3db732206c173acf283040408cb84f32cd5c
damien.rabois <damien.rabois@abc-culture.fr>
2024-01-31 11:13:30 +0100

    feat: add Levenshtein distance to check password similarity

---
+++ password_rotate/forms.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.forms import PasswordChangeForm
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+from rapidfuzz import fuzz
+from django.conf import settings
+
+
+class ForcePasswordChangeForm(PasswordChangeForm):
+    """
+    This form ensures the new password is not too similar to the old password.
+    """
+    def clean(self):
+        cleaned_data = super().clean()
+
+        ratio = fuzz.ratio(
+            self.cleaned_data["old_password"],
+            cleaned_data["new_password1"]
+        )
+        if ratio >= settings.PASSWORD_ROTATE_MAX_SIMILARITY_RATIO:
+            raise ValidationError(
+                {"new_password1": _("The new password is too similar to the old one.")},
+                code="password_similar"
+            )
+        return cleaned_data
--- password_rotate/views.py
+++ password_rotate/views.py
@@ -1,8 +1,18 @@
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.views import PasswordChangeView
 
+from password_rotate.forms import ForcePasswordChangeForm
+
 
 class ForcePasswordChangeView(PasswordChangeView):
+    """
+    This view forces the user to change her password when it expired.
+
+    Updating the password properly will change the password status from "expired"
+    to "valid".
+    """
+    form_class = ForcePasswordChangeForm
+
     def form_valid(self, form):
         form.save()
         # Updating the password logs out all other sessions for the user
